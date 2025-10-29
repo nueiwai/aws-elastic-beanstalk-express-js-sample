81dc9a44b7c0499042e96f8829c81a90a4901fb0
Nu Ei Wai <156038808+nueiwai@users.noreply.github.com>
2025-10-29 14:20:53 -0700

    add

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('COMP 4964 - Lab 7 - Part 2 -CI/CD github'));
+app.get('/', (req, res) => res.send('COMP 4964 - Lab 7 - Part 2 -CI/CD github -added stage'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
